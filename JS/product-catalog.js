eeb47baacb3b05077f27fc2da7951fefa09b3409
Mohammed Ali Elkhaddar <elkhaddarmehammedali@gmail.com>
2026-09-06 22:28:49 +0100

    update

--- JS/product-catalog.js
+++ JS/product-catalog.js
@@ -729,6 +729,14 @@ const PRODUCT_CATALOG = [
     "link": "/T-Shirts/product-tshirt56.html"
   },
   {
+    "id": "/T-Shirts/product-tshirt57.html",
+    "category": "tshirt",
+    "name": "Driven By Niyya Heavyweight Oversized Tee",
+    "price": "24.99",
+    "image": "/T-Shirts/T-Shirt57².jpg",
+    "link": "/T-Shirts/product-tshirt57.html"
+  },
+  {
     "id": "/Packs/product-pack1.html",
     "category": "pack",
     "name": "\"THE BOSS / THE REAL BOSS\" MATCHING PACK",
